1771d3e01d8cb791c3e18f17d3a60c89e41ac8d2
PapaZrix <zrilic313@gmail.com>
2022-09-18 18:36:07 +0200

    Add all math functions to the calculator

--- script.js
+++ script.js
@@ -8,3 +8,81 @@ const equals = document.querySelector(".equals");
 const previousOperand = document.querySelector(".previous-operand");
 const currentOperand = document.querySelector(".current-operand");
 const clear = document.querySelector(".clear");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Math operators
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "x":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
